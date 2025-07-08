3284fd75a51151143985b7045178bec7beee44db
Tùng Dương <kaduong240@gmail.com>
2025-07-08 22:47:21 +0700

    Lan dau

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public'))); // ✅
+
+app.engine('hbs', exphbs.engine({
+  extname: '.hbs',
+})); // ✅
+app.set('view engine', 'hbs');
+
+app.set('views', path.join(__dirname, 'resource/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
